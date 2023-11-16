1bc5ec53dacf08e52ec46f10c4f8f49752d9e9af
David Villafranca <djvphil413@gmail.com>
2023-11-16 15:08:10 -0500

    updating files

---
+++ scripts/cookie.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+
+if (firstName) {
+    document.cookie = "firstName=" + firstName + "; expires=Thu, 31 Dec 2023 23:59:59 UTC; path=/";
+    alert("Hello, " + firstName + "! Your name has been stored as a cookie.");
+}
+else {
+    alert("You didn't enter a name. Please try again.");
+}
