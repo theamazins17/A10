a69fff4315767ce78fa035d94692e1ff8bd6b1bf
David Villafranca <djvphil413@gmail.com>
2023-11-20 09:47:06 -0500

    updating files

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -1,9 +1,42 @@
-var firstName = prompt("What is your first name?");
-
-if (firstName) {
-    document.cookie = "firstName=" + firstName + "; expires=Thu, 31 Dec 2023 23:59:59 UTC; path=/";
-    alert("Hello, " + firstName + "!");
+// var firstName = prompt("What is your first name?");
+//
+// if (firstName) {
+//     document.cookie = "firstName=" + firstName + "; expires=Thu, 31 Dec 2023 23:59:59 UTC; path=/";
+//     alert("Hello, " + firstName + "!");
+// }
+// else {
+//     alert("You didn't enter a name. Please try again.");
+// }
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
-else {
-    alert("You didn't enter a name. Please try again.");
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
 }
